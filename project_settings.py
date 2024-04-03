03877a7e85b11ae91de286793ed52dafa70a037a
fravarcas <xxraptor51esp@gmail.com>
2024-04-03 20:04:00 +0200

    fix:improved validators

--- project_settings.py
+++ project_settings.py
@@ -1,12 +1,12 @@
-#Propiedades mario
+#Mario properties
 
 MAX_HORIZONTAL_JUMP_LENGTH = 11
 MAX_VERTICAL_JUMP_LENGTH= 4
 
-#Parametros AE_Generator
+#AE_GEnerator parametres
 
-MUTATION_PROBABILITY = 0.05
-NUMBER_OF_GENERATIONS = 2
+MUTATION_PROBABILITY = 0.0
+NUMBER_OF_GENERATIONS = 50
 NUMBER_OF_PARENTS = 2
 SOL_PER_POP = 5
 CROSSOVER_TYPE = "single_point"
@@ -14,3 +14,8 @@ PARENT_SELECTION_TYPE = "random"
 MUTATION_BY_REPLACEMENT = True
 MUTATION_TYPE = "random"
 GENE_SPACE = [x for x in range(1, 35)]
+
+#validator settings
+
+GROUND_CHARACTERS = {'X', '#', 'B', '?', 'Q', '%', 'b', '!', 'D', 'S', 'C', 'U', 'L', 't', 'T'}
+NON_WALKABLE_CHARACTERS = {'y', 'Y', 'E', 'g', 'k', 'K', 'r', '|', '1', '2', 'D', '1', '2', 'o', '-'}
--- validators/validadores_simples.py
+++ validators/validadores_simples.py
@@ -14,6 +14,29 @@ def transpose_file(input_file):
    
     return matriz_transpuesta
 
+def count_wall_length(row, ground_characters):
+    
+    wall_length = 0
+    try:
+        gap_index = row.index('-')
+    except ValueError:
+        gap_index = -1
+    
+    if gap_index == -1:
+        return len(row)
+    else:
+        for char in ground_characters:
+            wall_length += row[:gap_index].count(char)
+    return wall_length
+
+def read_level_file(file_path):
+    """
+    Lee el archivo de nivel y lo convierte en una matriz.
+    """
+    with open(file_path, 'r') as file:
+        level_matrix = [line.strip() for line in file]
+    return level_matrix
+
 #-----------Validadores fichero txt------------------
 
 def validate_beatable_pit(lvl):
@@ -25,11 +48,11 @@ def validate_beatable_pit(lvl):
 
     #Comprobar longitud de los fosos del mapa
     for i, transpose_row in enumerate(transpose_matrix):
-        if 'X' in transpose_matrix[i - 1] and set(transpose_row) == {'-'}:
+        if any(char in GROUND_CHARACTERS for char in transpose_matrix[i - 1]) and set(transpose_row) == {'-'}:
             row_count = 0
-        if set(transpose_row) == {'-'}:
+        if all(char in NON_WALKABLE_CHARACTERS for char in transpose_row):
             row_count += 1
-        if set(transpose_row) == {'-'} and 'X' in transpose_matrix[i + 1]:
+        if all(char in NON_WALKABLE_CHARACTERS for char in transpose_row) and any(char in GROUND_CHARACTERS for char in transpose_matrix[i + 1]):
             pit_list.append(row_count)
 
     #Comprobar si alguna de las longitudes es mayor que la distancia de salto de mario
@@ -42,29 +65,24 @@ def validate_beatable_pit(lvl):
 
 def validate_beatable_walls(lvl):
 
-    transpose_matrix = transpose_file(lvl)
+    lvl_matrix = read_level_file(lvl)
+    lvl_matrix_next_columns = [entry[1:] for entry in lvl_matrix]
     non_beatable_wall_count = 0
 
-    for i in range(len(transpose_matrix) - 1):
-
-        if transpose_matrix[i][::-1].find('X') == -1 or transpose_matrix[i + 1][::-1].find('X') == -1:
-            pass
-        
-        else:
-            #longitud de la pared en la columna actual
-            inverted_row = transpose_matrix[i][::-1]
-            find_gap = inverted_row.find('-')
-            wall_length = inverted_row[:find_gap].count('X')
-
-            #longitud de la pared en la columna siguiente
-            next_inverted_row = ''.join(reversed(transpose_matrix[i + 1]))
-            find_next_gap = next_inverted_row.find('-')
-            longitud_pared_siguiente = next_inverted_row[:find_next_gap].count('X')
-
-            #Comprueba si la diferencia de longitudes de las paredes es mayor que el salto más alto de mario
-            if (wall_length - longitud_pared_siguiente) > MAX_VERTICAL_JUMP_LENGTH:
+    for previous_column, current_column in zip(zip(*lvl_matrix), zip(*lvl_matrix_next_columns)):
+        if any(char in GROUND_CHARACTERS for char in current_column):
+            current_wall_length = count_wall_length(current_column[::-1], GROUND_CHARACTERS)
+            previous_wall_length = count_wall_length(previous_column[::-1], GROUND_CHARACTERS)
+            if current_wall_length - previous_wall_length > MAX_VERTICAL_JUMP_LENGTH:
                 non_beatable_wall_count += 1
 
     return non_beatable_wall_count
+
+if __name__== '__main__':
+
+    lvl = 'levels/originals/lvl_1-1.txt'
+    validate_beatable_walls(lvl)
+    
+    
 
 
